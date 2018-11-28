701e2e0c6ca6ce2604745bc78e7f25c3e5c5b633
Satoshi Ohmori <sachin21dev@gmail.com>
2018-11-29 01:18:49 +0900

    Drop support of Sprockets 2.x

--- lib/sprockets/webp/railtie.rb
+++ lib/sprockets/webp/railtie.rb
@@ -3,15 +3,17 @@
 module Sprockets
   module WebP
     class Railtie < ::Rails::Railtie
-      initializer :webp do |app|
-        app.assets.register_mime_type 'image/jpeg', '.jpg'
-        app.assets.register_postprocessor 'image/jpeg', :jpeg_webp do |context, data|
-          Converter.process(app, context, data)
-        end
+      initializer :webp, group: :all do |app|
+        app.config.assets.configure do |env|
+          env.register_mime_type 'image/jpeg', '.jpeg'
+          env.register_postprocessor 'image/jpeg', :jpeg_webp do |context, data|
+            Converter.process(app, context, data)
+          end
 
-        app.assets.register_mime_type 'image/png', '.png'
-        app.assets.register_postprocessor 'image/png', :png_webp do |context, data|
-          Converter.process(app, context, data)
+          env.register_mime_type 'image/png', '.png'
+          env.register_postprocessor 'image/png', :png_webp do |context, data|
+            Converter.process(app, context, data)
+          end
         end
       end
     end
