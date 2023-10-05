83eec914e71ebae41d52f421705b0191d0bcbf94
Konrad Makowski <konrad@snopkow.eu>
2023-10-05 13:37:46 +0200

    fix typo

--- lib/sprockets/webp/railtie.rb
+++ lib/sprockets/webp/railtie.rb
@@ -5,12 +5,12 @@ module Sprockets
     class Railtie < ::Rails::Railtie
       initializer :webp, group: :all do |app|
         app.config.assets.configure do |env|
-          evn.register_transformer 'image/jpeg', 'image/webp', Converter
+          env.register_transformer 'image/jpeg', 'image/webp', Converter
           env.register_bundle_postprocessor 'image/jpeg', :jpeg_webp do |context, data|
             Converter.process(app, context, data)
           end
 
-          env.register_mime_type 'image/png', '.png'
+          evn.register_mime_type 'image/png', '.png'
           evn.register_transformer 'image/png', 'image/webp', Converter
           env.register_bundle_postprocessor 'image/png', :png_webp do |context, data|
             Converter.process(app, context, data)
