6fbb6863eb14189b7fe6de4d0935cbaa8c94456e
Konrad Makowski <konrad@snopkow.eu>
2023-10-05 13:51:05 +0200

    fix & bump version

--- lib/sprockets/webp/railtie.rb
+++ lib/sprockets/webp/railtie.rb
@@ -6,13 +6,13 @@ module Sprockets
       initializer :webp, group: :all do |app|
         app.config.assets.configure do |env|
           env.register_transformer 'image/jpeg', 'image/webp', Converter
-          env.register_bundle_postprocessor 'image/jpeg', :jpeg_webp do |context, data|
+          env.register_postprocessor 'image/jpeg', :jpeg_webp do |context, data|
             Converter.process(app, context, data)
           end
 
           env.register_mime_type 'image/png', '.png'
           env.register_transformer 'image/png', 'image/webp', Converter
-          env.register_bundle_postprocessor 'image/png', :png_webp do |context, data|
+          env.register_postprocessor 'image/png', :png_webp do |context, data|
             Converter.process(app, context, data)
           end
         end
--- lib/sprockets/webp/version.rb
+++ lib/sprockets/webp/version.rb
@@ -2,6 +2,6 @@
 
 module Sprockets
   module WebP
-    VERSION = '0.4.6'
+    VERSION = '0.4.7'
   end
 end
