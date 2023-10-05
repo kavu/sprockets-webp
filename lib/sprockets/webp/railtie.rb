5f77567d6c6ef6e6b89a2d2e103029ce504abc85
Konrad Makowski <konrad@snopkow.eu>
2023-10-05 13:24:01 +0200

    Update processor

--- lib/sprockets/webp/railtie.rb
+++ lib/sprockets/webp/railtie.rb
@@ -1,17 +1,18 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
 module Sprockets
   module WebP
     class Railtie < ::Rails::Railtie
       initializer :webp, group: :all do |app|
         app.config.assets.configure do |env|
-          #  env.register_mime_type 'image/jpeg', '.jpeg'
-          env.register_postprocessor 'image/jpeg', :jpeg_webp do |context, data|
+          evn.register_transformer 'image/jpeg', 'image/webp', Converter
+          env.register_bundle_postprocessor 'image/jpeg', :jpeg_webp do |context, data|
             Converter.process(app, context, data)
           end
 
           env.register_mime_type 'image/png', '.png'
-          env.register_postprocessor 'image/png', :png_webp do |context, data|
+          evn.register_transformer 'image/png', 'image/webp', Converter
+          env.register_bundle_postprocessor 'image/png', :png_webp do |context, data|
             Converter.process(app, context, data)
           end
         end
