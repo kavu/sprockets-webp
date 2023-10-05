603ce9c6803c7fea48614058e524ab437c8206c8
Konrad Makowski <konrad@snopkow.eu>
2023-10-05 14:57:42 +0200

    try other approach

--- lib/sprockets/webp/converter.rb
+++ lib/sprockets/webp/converter.rb
@@ -9,79 +9,82 @@ require 'fastimage'
 module Sprockets
   module WebP
     class Converter
-      class << self
+      attr_reader :context
 
-        attr_reader :context
-
-        def process(app, context, data)
-          @context = context
-          # Application Config alias
-          config = app.config.assets
-
-          # If Application Assets Digests enabled - Add Digest
-          webp_file = webp_file_by_config(config, data)
-
-          # WebP File Pathname
-          webp_path = Pathname.new File.join(app.root, 'public', config.prefix, webp_file)
-
-          # Create Directory for both Files, unless already exists
-          FileUtils.mkdir_p(webp_path.dirname) unless Dir.exists?(webp_path.dirname)
-
-          # encode to webp
-          encode_to_webp(data, webp_path.to_path, webp_file) unless ::File.exist?(webp_path.to_path)
-
-          data
-        end
-
-        private
-
-        def webp_file_by_config(config, data)
-          digest    = config.digest ? "-#{context.environment.digest_class.new.update(data).to_s}" : nil
-          file_name = context.logical_path # Original File name w/o extension
-          file_ext  = context.pathname.extname # Original File extension
-          full_file_name = context.pathname.to_path
-
-          "#{file_name}#{digest}#{dimensions(full_file_name)}#{file_ext}.webp" # WebP File fullname
-        end
-
-        def dimensions(filename)
-          d = image_width_x_height(filename)
-
-          "#{'-' if d}#{d}"
-        end
-
-        def image_width_x_height(filename)
-          FastImage.size(filename)&.join('x')
-        end
-
-        def encode_to_webp(data, webp_path, webp_file = "")
-          # Create Temp File with Original File binary data
-          Tempfile.open('webp') do |file|
-            file.binmode
-            file.write(data)
-            file.close
-
-            # Encode Original File Temp copy to WebP File Pathname
-            begin
-              ::WebP.encode(file.path, webp_path, Sprockets::WebP.encode_options)
-              logger.info "Webp converted image #{webp_path}"
-            rescue => e
-              logger.warn "Webp convertion error of image #{webp_file}. Error info: #{e.message}"
-            end
-          end
-        end
-
-        def logger
-          if @context && @context.environment
-            @context.environment.logger
-          else
-            logger = Logger.new($stderr)
-            logger.level = Logger::FATAL
-            logger
-          end
-        end
-
+      def initialize(_options)
       end
+
+      def call(input)
+        app = input[:environment].context_class.app
+        @context = input[:environment].context_class.new(input)
+        data = input[:data]
+
+        # Application Config alias
+        config = app.config.assets
+
+        # If Application Assets Digests enabled - Add Digest
+        webp_file = webp_file_by_config(config, data)
+
+        # WebP File Pathname
+        webp_path = Pathname.new File.join(app.root, 'public', config.prefix, webp_file)
+
+        # Create Directory for both Files, unless already exists
+        FileUtils.mkdir_p(webp_path.dirname) unless Dir.exists?(webp_path.dirname)
+
+        # encode to webp
+        encode_to_webp(data, webp_path.to_path, webp_file) unless ::File.exist?(webp_path.to_path)
+
+        data
+      end
+
+      private
+
+      def webp_file_by_config(config, data)
+        digest    = config.digest ? "-#{context.environment.digest_class.new.update(data).to_s}" : nil
+        file_name = context.logical_path # Original File name w/o extension
+        file_ext  = context.pathname.extname # Original File extension
+        full_file_name = context.pathname.to_path
+
+        "#{file_name}#{digest}#{dimensions(full_file_name)}#{file_ext}.webp" # WebP File fullname
+      end
+
+      def dimensions(filename)
+        d = image_width_x_height(filename)
+
+        "#{'-' if d}#{d}"
+      end
+
+      def image_width_x_height(filename)
+        FastImage.size(filename)&.join('x')
+      end
+
+      def encode_to_webp(data, webp_path, webp_file = "")
+        # Create Temp File with Original File binary data
+        Tempfile.open('webp') do |file|
+          file.binmode
+          file.write(data)
+          file.close
+
+          # Encode Original File Temp copy to WebP File Pathname
+          begin
+            ::WebP.encode(file.path, webp_path, Sprockets::WebP.encode_options)
+            logger.info "Webp converted image #{webp_path}"
+          rescue => e
+            logger.warn "Webp convertion error of image #{webp_file}. Error info: #{e.message}"
+          end
+        end
+      end
+
+      def logger
+        if @context && @context.environment
+          @context.environment.logger
+        else
+          logger = Logger.new($stderr)
+          logger.level = Logger::FATAL
+          logger
+        end
+      end
+
     end
   end
 end
--- lib/sprockets/webp/railtie.rb
+++ lib/sprockets/webp/railtie.rb
@@ -6,15 +6,11 @@ module Sprockets
       initializer :webp, group: :all do |app|
         app.config.assets.configure do |env|
           env.register_transformer 'image/jpeg', 'image/webp', Converter
-          env.register_postprocessor 'image/jpeg', :jpeg_webp do |context, data|
-            Converter.process(app, context, data)
-          end
+          env.register_postprocessor 'image/jpeg', Converter.new
 
           env.register_mime_type 'image/png', '.png'
           env.register_transformer 'image/png', 'image/webp', Converter
-          env.register_postprocessor 'image/png', :png_webp do |context, data|
-            Converter.process(app, context, data)
-          end
+          env.register_postprocessor 'image/png', Converter.new
         end
       end
     end
